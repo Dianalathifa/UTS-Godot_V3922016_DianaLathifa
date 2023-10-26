ae7b282845fa16b850865875f0eda401a2d91282
unknown <dl.lathif1803@student.uns.ac.id>
2023-10-26 10:34:20 +0700

    Main Menu

--- main_menu.gd
+++ main_menu.gd
@@ -3,8 +3,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("MainMenu ready!")
-
+	print("MainMenu Hi")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -13,3 +12,4 @@ func _process(delta):
 
 func _on_start_button_pressed():
 	get_tree().change_scene_to_file("res://level_1.tscn")
+	
